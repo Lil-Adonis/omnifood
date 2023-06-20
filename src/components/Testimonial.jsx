4cf620a24703c209cd3a53dbd0af830a36a17b90
Lil-Adonis <adenugaib24@gmail.com>
2023-06-20 09:32:14 -1100

    testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Testimonial = () => {
+const Testimonial = ({ image, text, name }) => {
   return (
-    <div>Testimonial</div>
-  )
-}
+    <Wrapper>
+      <figure className="testimonial">
+        <img src={image} className="testimonial-img" alt={`customer ${name}`} />
+        <blockquote className="testimonial-text">{text}</blockquote>
+        <p className="testimonial-name">&mdash; {name}</p>
+      </figure>
+    </Wrapper>
+  );
+};
 
-export default Testimonial
+const Wrapper = styled.figure`
+  .testimonial-img {
+    width: 6.4rem;
+    border-radius: 50%;
+    margin-bottom: 1.2rem;
+  }
+
+  .testimonial-text {
+    font-size: 1.8rem;
+    line-height: 1.8;
+    margin-bottom: 1.6rem;
+  }
+
+  .testimonial-name {
+    font-size: 1.6rem;
+    color: #6f6f6f;
+  }
+`;
+
+export default Testimonial;
