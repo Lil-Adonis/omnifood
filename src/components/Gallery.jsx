a70cd9e1329dab5048d951c5fdf43ef974055df3
Lil-Adonis <adenugaib24@gmail.com>
2023-06-20 09:49:30 -1100

    Gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const Gallery = () => {
+const Gallery = ({ image }) => {
   return (
-    <div>Gallery</div>
-  )
-}
+    <Wrapper>
+      <img src={image} alt="arranged food" />
+    </Wrapper>
+  );
+};
 
-export default Gallery
+const Wrapper = styled.figure`
+  overflow: hidden;
+  & img {
+    display: block;
+    width: 100%;
+    transition: all 0.4s;
+  }
+  & img:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export default Gallery;
