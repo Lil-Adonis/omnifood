17f2751605425f4ba0b61da4baaa7906d72402bb
Lil-Adonis <adenugaib24@gmail.com>
2023-06-21 11:03:04 -1100

    error

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="container center-text">
+      <h4 className="heading-secondary">error</h4>
+    </div>
+  );
+};
+
+export default Error;
