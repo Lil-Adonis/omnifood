b614e85b02d8b2e8186fe451fd6f2bf06cf83665
Lil-Adonis <adenugaib24@gmail.com>
2023-06-21 11:06:53 -1100

    gridview

--- src/components/GridView.jsx
+++ src/components/GridView.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Meal from "./Meal";
 
-const GridView = () => {
+const GridView = ({ meals }) => {
+  const tempMeals = [...meals];
   return (
-    <div>GridView</div>
-  )
-}
+    <div className="container grid grid--3-cols margin-bottom-md">
+      {tempMeals.slice(0, 9).map((meal) => {
+        return <Meal key={meal.idMeal} {...meal} />;
+      })}
+    </div>
+  );
+};
 
-export default GridView
+export default GridView;
