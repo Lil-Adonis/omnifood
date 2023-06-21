1328db85aa10a89a899a3422043a7bb6d76bc3b1
Lil-Adonis <adenugaib24@gmail.com>
2023-06-21 10:58:22 -1100

    customers images

--- src/components/CustomersImages.jsx
+++ src/components/CustomersImages.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { customer_images } from "../utils/constants";
 
 const CustomersImages = () => {
   return (
-    <div>CustomersImages</div>
-  )
-}
+    <div className="delivered-meals">
+      <div className="delivered-imgs">
+        {customer_images.map((img, index) => {
+          return <img key={index} src={img} alt={`Customer ${index}`} />;
+        })}
+      </div>
+      <p className="delivered-text">
+        <span>250,000+</span> meals delivered last year!
+      </p>
+    </div>
+  );
+};
 
-export default CustomersImages
+export default CustomersImages;
