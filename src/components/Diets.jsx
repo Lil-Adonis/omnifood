01771373f94490d27bea6f77a2c5616f1c2f1366
Lil-Adonis <adenugaib24@gmail.com>
2023-06-21 11:02:06 -1100

    diet

--- src/components/Diets.jsx
+++ src/components/Diets.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { IoCheckmarkOutline } from "react-icons/io5";
 
 const Diets = () => {
   return (
-    <div>Diets</div>
-  )
-}
+    <div className="diets">
+      <h3 className="heading-tertiary">Works with any diet:</h3>
+      <ul className="list">
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Vegetarian</span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Vegan </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Pescatarian </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Gluten-free </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Lactose-free </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Keto </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Paleo </span>
+        </li>
+        <li className="list-item">
+          <span className="list-icon">
+            <IoCheckmarkOutline />
+          </span>
+          <span>Kid-friendly </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Diets
+export default Diets;
