1383da38b503b7231e5462380d197083e7640cda
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:38:39 -1100

    meals page

--- src/pages/MealsPage.jsx
+++ src/pages/MealsPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import MealsList from "../components/MealsList";
+import Filters from "../components/Filters";
 const MealsPage = () => {
   return (
-    <div>MealsPage</div>
-  )
-}
+    <Wrapper>
+      <div className="container">
+        <span className="subheading">Meals</span>
+        <h2 className="heading-secondary">
+          Omnifood AI chooses from 5,000+ recipes
+        </h2>
+      </div>
+      <Filters />
+      <div>
+        <MealsList />
+      </div>
+    </Wrapper>
+  );
+};
 
-export default MealsPage
+const Wrapper = styled.section`
+  padding: 9.6rem 0;
+`;
+
+export default MealsPage;
