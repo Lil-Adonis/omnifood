e909b3a6bcf7d5e7801b6fe29484d19e210693a3
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:43:46 -1100

    Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { MealsProvider } from "./context/Meals_context";
+import { FilterProvider } from "./context/Filter_context";
+import { UserProvider } from "./context/User_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={window.location.origin}
+  >
+    <UserProvider>
+      <MealsProvider>
+        <FilterProvider>
+          <App />
+        </FilterProvider>
+      </MealsProvider>
+    </UserProvider>
+  </Auth0Provider>,
 )
