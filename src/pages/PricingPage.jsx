81b3fc5daa24a5250f8693cfb203ed570d9c0c37
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:39:14 -1100

    pricing page

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Pricing from "../components/Pricing";
+import PricingDetails from "../components/PricingDetails";
+import PricingFeatures from "../components/PricingFeatures";
 
 const PricingPage = () => {
   return (
-    <div>PricingPage</div>
-  )
-}
+    <Wrapper className="section-pricing">
+      <div className="container">
+        <span className="subheading">Pricing</span>
+        <h2 className="heading-secondary">
+          Eating well without breaking the bank
+        </h2>
+      </div>
+      <Pricing />
+      <PricingDetails />
+      <PricingFeatures />
+    </Wrapper>
+  );
+};
 
-export default PricingPage
+const Wrapper = styled.section`
+  padding: 9.6rem 0;
+`;
+
+export default PricingPage;
