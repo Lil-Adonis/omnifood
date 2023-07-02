4e1faae0808f8a1a5a1b5f61c20aa5d271e49f47
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:37:26 -1100

    Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  Hero,
+  Featured,
+  HowItWorks,
+  Testimonials,
+  FeaturedMeals,
+} from "../components";
 
-const Homepage = () => {
+const HomePage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <main>
+      <Hero />;
+      <Featured />
+      <HowItWorks />
+      <FeaturedMeals />
+      <Testimonials />
+    </main>
+  );
+};
 
-export default Homepage
+export default HomePage;
