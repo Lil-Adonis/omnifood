36622e07567745ff612fa6364a38c5719077a165
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:32:10 -1100

    Mealslist && .env

--- src/components/MealsList.jsx
+++ src/components/MealsList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useFilterContext } from "../context/Filter_context";
+import GridView from "./GridView";
 
 const MealsList = () => {
-  return (
-    <div>MealsList</div>
-  )
-}
+  const { filtered_meals: meals } = useFilterContext();
 
-export default MealsList
+  if (meals.length < 1) {
+    return (
+      <h5 className="subheading" style={{ textTransform: "none" }}>
+        Sorry, no products matched your search
+      </h5>
+    );
+  }
+
+  return <GridView meals={meals} />;
+};
+
+export default MealsList;
