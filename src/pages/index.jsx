428b5f519061cbbdaa543bd6b35736bee8e76ed7
Lil-Adonis <adenugaib24@gmail.com>
2023-07-02 04:38:07 -1100

    page index

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import Home from "./HomePage";
+import Meals from "./MealsPage";
+import Error from "./ErrorPage";
+import Pricing from "./PricingPage";
+import CtaPage from "./CtaPage";
+import AuthWrapper from "./AuthWrapper";
+
+export { Home, Meals, Error, Pricing, CtaPage, AuthWrapper };
